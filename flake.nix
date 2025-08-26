0aaf76f5ca6994df030dbfe4d462afedd8da33e5
Deathraymind <deathraymind@gmail.com>
2025-08-26 19:21:23 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Rust dev shell with latest stable via rust-overlay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.rust-bin.stable.latest.default   # <- rustc/cargo fmt/clippy at latest stable
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        };
+      });
+}
+
